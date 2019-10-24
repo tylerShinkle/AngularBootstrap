3a3baef09fbd33ad1fdb09ed1853bd4f64655f03
tylerShinkle <shinklt1@gmatc.matc.edu>
2019-10-24 14:27:23 -0500

    bootStrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directives';
+  courses = [];
+  viewMode = 'map';
+}
+
+
+//STRUCTURAL DIRECTIVES : modify the structure of the DOM
+
+//ATTRIBUTE DIRECTIVES : modify the attributes of DOM elements
+
+//https://youtu.be/LtT01ZCHRjk?list=PLTjRvDozrdlxAhsPP4ZYtt3G8KbJ449oT&t=1007
+
